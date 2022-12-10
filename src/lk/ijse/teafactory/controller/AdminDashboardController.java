25e3fb381284a0df1c8518e8e8aae1760ef2a045
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-10 22:53:46 +0530

    Add content

--- src/lk/ijse/teafactory/controller/AdminDashboardController.java
+++ src/lk/ijse/teafactory/controller/AdminDashboardController.java
@@ -1,4 +1,35 @@
 package lk.ijse.teafactory.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminDashboardController {
+    public JFXButton btnCustomers;
+    public JFXButton btnStocks;
+    public JFXButton btnOrders;
+    public JFXButton btnProducts;
+    public JFXButton btnSuppliers;
+    public JFXButton btnHome;
+    public Label lblAdminDashboard;
+    public AnchorPane pane;
+
+    public void btnHomeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSuppliersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnProductsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrdersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStocksOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomersOnAction(ActionEvent actionEvent) {
+    }
 }
