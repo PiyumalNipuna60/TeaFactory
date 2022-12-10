cf52f139564b35f5a22aa5f5ee978d5d8d793b9d
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-10 23:14:23 +0530

    Add content

--- src/lk/ijse/teafactory/controller/LoginFormController.java
+++ src/lk/ijse/teafactory/controller/LoginFormController.java
@@ -1,4 +1,21 @@
 package lk.ijse.teafactory.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane pane;
+    public Label lglLogin;
+    public Button btnLogin;
+    public PasswordField txtPassword;
+    public TextField txtUserName;
+    public Label lblPassword;
+    public Label lblUserName;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
