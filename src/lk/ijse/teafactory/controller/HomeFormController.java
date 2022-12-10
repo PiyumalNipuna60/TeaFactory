857b1868b85dd04d2622b39f7df5e79230f07c2b
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-10 23:03:46 +0530

    Add content

--- src/lk/ijse/teafactory/controller/HomeFormController.java
+++ src/lk/ijse/teafactory/controller/HomeFormController.java
@@ -1,4 +1,16 @@
 package lk.ijse.teafactory.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class HomeFormController {
+    public Button btnLogin;
+    public Label lblFactoryName;
+    public AnchorPane pane;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+
+    }
 }
