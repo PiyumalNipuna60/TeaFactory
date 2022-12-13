c2d7aa0edef14a9aae1d5c6cd2b0b243602bb7e2
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-13 22:14:20 +0530

    Add Content

--- src/lk/ijse/teafactory/controller/SupplierFormControllerController.java
+++ src/lk/ijse/teafactory/controller/SupplierFormControllerController.java
@@ -1,4 +1,75 @@
 package lk.ijse.teafactory.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SupplierFormControllerController {
+    public AnchorPane pane;
+    public JFXTextField txtSupplierId;
+    public JFXTextField txtSupplierName;
+    public JFXTextField txtDescription;
+    public JFXButton btnAddSupplier;
+    public JFXTextField txtSearch;
+    public JFXButton btnSearchSupplier;
+    public TableView tblSupplier;
+    public TableColumn colSupplierId;
+    public TableColumn colSupplierName;
+    public TableColumn colDescription;
+    public TableColumn colQty;
+    public JFXTextField txtQty;
+    public JFXButton btnLogin;
+    public JFXButton btnProducts;
+    public JFXButton btnCustomers;
+    public JFXButton btnOrders;
+    public JFXButton btnUpdateSupplier;
+    public JFXButton btnStocks;
+    public JFXButton btnDeleteSupplier;
+    public JFXButton btnClear;
+
+    public void txtSupplierIdOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void RowClicked(MouseEvent mouseEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSuppliersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnProductsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCustomersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOrdersOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStocksOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteSupplierOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnclearOnAction(ActionEvent actionEvent) {
+    }
 }
