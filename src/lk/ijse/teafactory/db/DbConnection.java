bc0cd762801151aa0cff7e775691e986ec6ebe2c
PiyumalNipuna60 <nipuna315np@gmail.com>
2022-12-13 23:18:48 +0530

    Add DbConnection

---
+++ src/lk/ijse/teafactory/db/DbConnection.java
@@ -0,0 +1,27 @@
+package lk.ijse.teafactory.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static DbConnection dbConnection;
+    private Connection connection;
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/TeaFactory","root","1234");
+    }
+
+    public static DbConnection  getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection==null){
+            return dbConnection = new DbConnection();
+        }else {
+            return dbConnection;
+        }
+    }
+
+    private Connection getConnection(){
+        return connection;
+    }
+}
